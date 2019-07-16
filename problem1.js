d8285206f547ae1b061d81ae0bc595d6dc1c6cef
wpdlab <zlzlaosxj@naver.com>
2019-07-16 15:27:58 +0900

    sex

---
+++ problem1.js
@@ -0,0 +1,30 @@
+const { Api, JsonRpc, RpcError } = require('eosjs');
+
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');
+
+const { PrivateKey, PublicKey, Signature, Aes, key_utils, config } = require('eosjs-ecc');
+
+const { TextEncoder, TextDecoder } = require('util');
+
+const fetch = require('node-fetch');
+
+
+let privateKeys = [];// user private keys
+
+const rpc = new JsonRpc('http://192.168.1.75:8010', { fetch });
+
+const signatureProvider = new JsSignatureProvider(privateKeys);
+
+const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+
+async function main() {
+    try {
+        const result = await rpc.get_block(30000);
+        console.log(result.timestamp);
+        console.log(result.producer);
+    } catch(error) {
+        console.error(error);
+    }
+}
+main();
